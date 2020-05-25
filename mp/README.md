f74dff3eb9c434b2feaf39ff4b5ded6bd0cfb0b9
Tom-Lennox <dennkousekka1212@gmail.com>
2020-05-25 15:26:06 +0900

    build作成、mdファイル編集

--- mp/README.md
+++ mp/README.md
@@ -2,6 +2,34 @@ Vue CLI v4.3.1
 ? Please pick a preset: default (babel, eslint)
 ? Pick the package manager to use when installing dependencies: Yarn
 
+
+# ▼ assets:
+css, 画像等の格納。
+
+# ▼ main.js:
+最終的にcomponentsを1つのjsファイルにするもの(bundle.jsの立ち位置)
+
+# ▼ 【build】するには：
+```
+[package.json]
+  "scripts": {
+    # ▼ 開発用
+    "serve": "vue-cli-service serve",
+    # ▼ 本番用
+    "build": "vue-cli-service build",
+    "lint": "vue-cli-service lint"
+  },
+yarn build
+ ⇒ dist dirに書き出し。
+
+・[dist]内の解説
+(index.html, 最後の方に記載。)
+ - chunk-~~.js：
+ node_modulesの中身。
+ - app-~~.js：
+ 
+```
+
 <!-- == -->
 # init
 
--- mp/src/main.js
+++ mp/src/main.js
@@ -4,5 +4,25 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  // # ▼ 
+  // el: '#app' ⇒ .$mount('#app')
+  // #app ⇒ [index.html, <!-- built files will be auto injected -->の部分で読み込まれる。]
   render: h => h(App),
+  // # ▼ 
+  // render関数：ES5, ES6の違い。下記と意味は同じ。
+  // // '''
+  // render: function(h) {
+  //   return h(App)
+  // }
+  // or
+  // render: function(createElement) {
+  //   return createElement(App)
+  // }
+  // // '''
+  // # ▼ 
+  // render: h => h('div', 'こんにちは')
+  // （ESLint, 使用の際には「import App from './App.vue'」を削除のこと。）
+  // # ▼ 
+  // App ⇒ objを取る。
+  // component ⇒ importされた時点でobj。
 }).$mount('#app')
