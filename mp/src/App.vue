0bade7d28cff3dcaf75ef296decbbd2ad232cef2
Tom-Lennox <dennkousekka1212@gmail.com>
2020-05-29 08:27:57 +0900

    prettier導入

--- mp/src/App.vue
+++ mp/src/App.vue
@@ -1,12 +1,3 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <LikeNumber></LikeNumber>
 </template>
-
-<script>
-</script>
-
-<style>
-</style>
--- mp/src/main.js
+++ mp/src/main.js
@@ -1,15 +1,21 @@
+// # ▼ component、グローバル登録する場合：
+// 記：[main.js]
+// 手：import, Vue.component
 import Vue from 'vue'
 import App from './App.vue'
+import LikeNumber from './LikeNumber.vue'
 
 Vue.config.productionTip = false
+Vue.component("LikeNumber", LikeNumber)
 
 new Vue({
-  // # ▼ 
+  // # ▼ .$mount('#app')の意味：el: '#app' と同じ
+
   // el: '#app' ⇒ .$mount('#app')
   // #app ⇒ [index.html, <!-- built files will be auto injected -->の部分で読み込まれる。]
   render: h => h(App),
-  // # ▼ 
-  // render関数：ES5, ES6の違い。下記と意味は同じ。
+
+  // # ▼ render関数：ES5, ES6の違い。下記と意味は同じ。
   // // '''
   // render: function(h) {
   //   return h(App)
@@ -19,10 +25,12 @@ new Vue({
   //   return createElement(App)
   // }
   // // '''
+
   // # ▼ 
   // render: h => h('div', 'こんにちは')
-  // （ESLint, 使用の際には「import App from './App.vue'」を削除のこと。）
-  // # ▼ 
+  // （ESLint, ※使用の際には「import App from './App.vue'」を削除のこと。）
+
+  // # ▼ importされたファイルはobj形式である。
   // App ⇒ objを取る。
   // component ⇒ importされた時点でobj。
 }).$mount('#app')
